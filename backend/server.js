aad934b16919e059ce55f6756f27704771793023
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-13 13:20:43 +0700

    Serve products from backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const products = require('./data/products');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Api is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  res.json(products.find((product) => product._id === req.params.id));
+});
+
+app.listen(5000, console.log('Server is running on port 5000'));
