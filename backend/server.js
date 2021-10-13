1afee03f9dcebbf6728df1b03c8f73cdf491f097
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-13 23:37:44 +0700

    fetch products from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
-import colors from 'colors';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import colors from 'colors';
 
 dotenv.config();
 
@@ -14,13 +14,15 @@ app.get('/', (req, res) => {
   res.send('Api is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+// app.get('/api/products', (req, res) => {
+//   res.json(products);
+// });
 
-app.get('/api/products/:id', (req, res) => {
-  res.json(products.find((product) => product._id === req.params.id));
-});
+// app.get('/api/products/:id', (req, res) => {
+//   res.json(products.find((product) => product._id === req.params.id));
+// });
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
