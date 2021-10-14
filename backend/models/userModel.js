36136ef143ff13322a7ad94eb9ef259b816113c5
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-14 19:20:55 +0700

    User registration & password hashing

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
+//Login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+//Register
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
