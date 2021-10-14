8c7c3fff124e6344da7191a8759c7b935a096bc4
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-14 18:45:26 +0700

    Create product controller, User auth & generate token

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,21 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Api is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 //middleware
 app.use(notFound);
