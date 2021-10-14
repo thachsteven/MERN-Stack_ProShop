fc9eff880bd2f32f43f537881f27cd089783f4a3
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-14 22:50:01 +0700

    Register function

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,11 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from '../constants/userConstants';
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from '../constants/userConstants';
 
 const initialState = {};
 
@@ -11,6 +18,22 @@ export const userLoginReducer = (state = initialState, action) => {
       return { loading: false, userInfo: action.payload };
 
     case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+
+    case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
 
     default:
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,55 +1,88 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Form, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../actions/userActions';
+import Message from './../components/Message';
+import Loading from './../components/Loading/Loading';
+import { history } from './../App';
 
 const RegisterScreen = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const { userInfo, loading, error } = useSelector((state) => state.userRegister);
+
+  useEffect(() => {
+    if (userInfo) {
+      history.goBack();
+    }
+  }, [userInfo]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Password does not match');
+    } else {
+      dispatch(register(name, email, password));
+    }
+  };
 
   return (
     <FormContainer>
       <h1>Sign Up</h1>
-      <Form>
+      {message && <Message>{message}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loading />}
+      <Form onSubmit={submitHandler}>
         <Form.Group controlId="name">
-          <Form.Label>Email Name</Form.Label>
-          <Form.Control onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Enter name" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
             type="email"
             placeholder="Enter email"
-          />
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="password">
-          <Form.Label>Enter Password</Form.Label>
+          <Form.Label>Password Address</Form.Label>
           <Form.Control
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Enter password"
-          />
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group controlId="confirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
             type="password"
             placeholder="Confirm password"
-          />
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
         <Button type="submit" variant="primary">
-          Sign Up
+          Register
         </Button>
       </Form>
       <Row className="py-3">
