19cf812953e5b36e016af6ea5b8b86fc3d65a812
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-15 18:53:37 +0700

    Order screen binding all

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -10,6 +10,8 @@ import {
 const initialState = {
   orderItems: [],
   shippingAddress: {},
+  loading: true,
+  order: [],
 };
 
 //Create order
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,38 +1,99 @@
-import React from 'react';
-import { Card, Col, ListGroup, Row } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Card, Col, ListGroup, Row, Image } from 'react-bootstrap';
 import Message from '../components/Message';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOrderDetails } from '../actions/orderActions';
+import { Link } from 'react-router-dom';
+import Loading from './../components/Loading/Loading';
 
-const OrderScreen = () => {
-  return (
+const OrderScreen = (props) => {
+  const orderId = props.match.params.id;
+  const dispatch = useDispatch();
+
+  const { order, loading, error } = useSelector((state) => state.orderDetails);
+
+  useEffect(() => {
+    dispatch(getOrderDetails(orderId));
+  }, [dispatch, orderId]);
+
+  if (!loading) {
+    //   Calculate prices
+    const addDecimals = (num) => {
+      return (Math.round(num * 100) / 100).toFixed(2);
+    };
+
+    order.itemsPrice = addDecimals(order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+    order.shippingPrice = addDecimals(order.itemsPrice > 100 ? 0 : 100);
+    order.taxPrice = addDecimals(Number((0.15 * order.itemsPrice).toFixed(2)));
+    order.totalPrice = (Number(order.itemsPrice) + Number(order.shippingPrice) + Number(order.taxPrice)).toFixed(2);
+  }
+
+  return loading ? (
+    <Loading />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
     <>
-      <h1>Order </h1>
+      <h1>Order {order._id}</h1>
       <Row>
         <Col md={8}>
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
-                <strong>Name: </strong>
+                <strong>Name: </strong> {order.user.name}
               </p>
               <p>
-                <strong>Email: </strong>
+                <strong>Email: </strong> <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
               </p>
               <p>
                 <strong>Address:</strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode},{' '}
+                {order.shippingAddress.country}
               </p>
-              <Message variant="success">Delivered on</Message>
+              {order.isDelivered ? (
+                <Message variant="success">Delivered on {order.deliveredAt}</Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Payment Method</h2>
               <p>
                 <strong>Method: </strong>
+                {order.paymentMethod}
               </p>
-              <Message variant="danger">Not Paid</Message>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Order Items</h2>
+              {order.orderItems.length === 0 ? (
+                <Message>Order is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image src={item.image} alt={item.name} fluid rounded />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
             </ListGroup.Item>
           </ListGroup>
         </Col>
@@ -45,21 +106,25 @@ const OrderScreen = () => {
               <ListGroup.Item>
                 <Row>
                   <Col>Items</Col>
+                  <Col>${order.itemsPrice}</Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
                 <Row>
                   <Col>Shipping</Col>
+                  <Col>${order.shippingPrice}</Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
                 <Row>
                   <Col>Tax</Col>
+                  <Col>${order.taxPrice}</Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
                 <Row>
                   <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
             </ListGroup>
